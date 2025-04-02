bbc0fb9a2284fae6c19c75af31071f26ac84be1d
FNP-git <devops@fnpglobal.com>
2025-04-03 03:20:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/form", formRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
