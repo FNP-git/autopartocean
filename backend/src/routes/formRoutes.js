0d070d8a8e39ff7a70b56493ec03bb97c9bff685
FNP-git <devops@fnpglobal.com>
2025-04-03 21:42:27 +0530

    Update formRoutes.js

--- backend/src/routes/formRoutes.js
+++ backend/src/routes/formRoutes.js
@@ -70,7 +70,7 @@ router.post("/", validateForm, async (req, res) => {
   // Email options including the hidden field value
   const mailOptions = {
     from: `"AutoPart Ocean Support" <devops@fnpglobal.com>`,
-    to: "autopartslead9@gmail.com",
+    to: "leads@autopartocean.com",
     subject: "New Form Submission AutoPart Ocean",
     text: 
       `Company": ${leadLabel}\n` +
